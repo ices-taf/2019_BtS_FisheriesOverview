3b187590f321e85a74aabc7ed270a0149e700fcd
Adriana Villamor <adriana.villamor@ices.dk>
2019-10-02 10:42:45 +0200

    csv's saved at the end of each script and imported in the start

--- data.R
+++ data.R
@@ -1,9 +1,8 @@
 
 # Initial formatting of the data
 
-require(dplyr)
 taf.library(icesFO)
-
+require(dplyr)
 mkdir("data")
 
 # 1: ICES official cath statistics
@@ -14,15 +13,28 @@ prelim <- read.taf("bootstrap/data/ICES_nominal_catches/ICES_preliminary_catches
 
 catch_dat <- icesFO::format_catches(2019, "Baltic Sea Ecoregion", hist, official, prelim)
 
+write.taf(catch_dat, file = "data/catch_dat.csv", quote = TRUE)
+
 # 2: STECF effort and landings
 
 effort <- read.taf("bootstrap/data/STECF_effort.csv")
-landings <- read.taf("bootstrap/data/STECF_landings.csv")
-
+library(readr)
+landings <- read_csv("bootstrap/initial/data/STECF_landings.csv")
 
 frmt_effort <- format_stecf_effort(effort)
-
+effort <- dplyr::rename(effort,
+                 regulated.area = `regulated area`,
+                 regulated.gear = `regulated gear`)
+frmt_effort <- format_stecf_effort(effort)
 frmt_landings <- format_stecf_landings(landings)
+names(landings)
+landings <- dplyr::rename(landings,
+                        regulated.area = `regulated area`,
+                        regulated.gear = `regulated gear`)
+frmt_landings <- format_stecf_landings(landings)
+
+write.taf(frmt_effort, file = "data/frmt_effort.csv", quote = TRUE)
+write.taf(frmt_landings, file = "data/frmt_landings.csv")
 
 # 3: SAG
 sag_sum <- read.taf("bootstrap/data/SAG_data/SAG_summary.csv")
@@ -39,4 +51,5 @@ clean_sag <- dplyr::filter(clean_sag, StockKeyLabel %!in% Baltic_Out_stocks)
 clean_status <- dplyr::filter(clean_status, StockKeyLabel %!in% Baltic_Out_stocks)
 detach("package:operators", unload=TRUE)
 
-
+write.taf(clean_sag, file = "data/clean_sag.csv")
+write.taf(clean_status, file = "data/clean_status.csv")
--- report.R
+++ report.R
@@ -5,11 +5,29 @@ library(icesTAF)
 
 mkdir("report")
 
+##########
+#Load data
+##########
+
+catch_dat <- read.taf("data/catch_dat.csv")
+
+frmt_effort <- read.taf("data/frmt_effort.csv")
+frmt_landings <- read.taf("data/frmt_landings.csv")
+trends <- read.taf("model/trends.csv")
+catch_current <- read.taf("model/catch_current.csv")
+catch_trends <- read.taf("model/catch_trends.csv")
+
+clean_status <- read.taf("data/clean_status.csv")
+
+areas <- read.taf("bootstrap/data/ICES_areas/ICES_areas.csv")
+ecoregions <- read.taf("bootstrap/data/ICES_ecoregions/ICES_ecoregions.csv")
+
 ###############
 ##Ecoregion map
 ###############
 
 plot_ecoregionMap()
+
 
 
 
@@ -22,11 +40,11 @@ plot_ecoregionMap()
 #~~~~~~~~~~~~~~~#
         #Plot
 plot_catch_trends(catch_dat, type = "COMMON_NAME", line_count = 5, plot_type = "line")
-ggplot2::ggsave("2019_BtS_FO_Figure5.png", path = "./report", width = 170, height = 100.5, units = "mm", dpi = 300)
+ggplot2::ggsave("2019_BtS_FO_Figure5.png", path = "report/", width = 170, height = 100.5, units = "mm", dpi = 300)
 
         #data
-BtS_catchby_commonname <- plot_catch_trends(catch_dat, type = "COMMON_NAME", line_count = 5, plot_type = "line", return_data = T)
-write.taf("2019_BtS_FO_Figure5.csv", dir = "report")
+dat <- plot_catch_trends(catch_dat, type = "COMMON_NAME", line_count = 5, plot_type = "line", return_data = T)
+write.taf(dat,"2019_BtS_FO_Figure5.csv", dir = "report")
 
 
 #~~~~~~~~~~~~~~~#
@@ -34,11 +52,11 @@ write.taf("2019_BtS_FO_Figure5.csv", dir = "report")
 #~~~~~~~~~~~~~~~#
         #Plot
 plot_catch_trends(catch_dat, type = "COUNTRY", line_count = 9, plot_type = "area")
-ggplot2::ggsave("2019_BtS_FO_Figure2.png", path = "~/", width = 178, height = 130, units = "mm", dpi = 300)
+ggplot2::ggsave("2019_BtS_FO_Figure2.png", path = "report/", width = 178, height = 130, units = "mm", dpi = 300)
 
         #data
-plot_dat<-plot_catch_trends(catch_dat, type = "COUNTRY", line_count = 9, plot_type = "area", return_data = T)
-write.taf(plot_dat, file= "2019_BtS_FO_Figure2.csv", dir = "report")
+dat<-plot_catch_trends(catch_dat, type = "COUNTRY", line_count = 9, plot_type = "area", return_data = T)
+write.taf(dat, file= "2019_BtS_FO_Figure2.csv", dir = "report")
 
 #~~~~~~~~~~~~~~~#
 # By guild
@@ -51,11 +69,11 @@ catch_dat2 <-dplyr::filter(catch_dat2, GUILD != "Elasmobranch")
 
         #Plot
 plot_catch_trends(catch_dat2, type = "GUILD", line_count = 4, plot_type = "line")
-ggplot2::ggsave("2019_BtS_FO_Figure4.png", path = "~/", width = 178, height = 130, units = "mm", dpi = 300)
+ggplot2::ggsave("2019_BtS_FO_Figure4.png", path = "report/", width = 178, height = 130, units = "mm", dpi = 300)
         
         #data
-plot_dat <- plot_catch_trends(catch_dat, type = "GUILD", line_count = 4, plot_type = "line", return_data = T)
-write.taf(plot_dat, file= "2019_BtS_FO_Figure4.csv", dir = "report")
+dat <- plot_catch_trends(catch_dat, type = "GUILD", line_count = 4, plot_type = "line", return_data = T)
+write.taf(dat, file= "2019_BtS_FO_Figure4.csv", dir = "report")
 
 ################################
 ## 2: STECF effort and landings#
@@ -65,33 +83,35 @@ write.taf(plot_dat, file= "2019_BtS_FO_Figure4.csv", dir = "report")
 # Effort by country
 #~~~~~~~~~~~~~~~#
         #Plot
-plot_stecf(frmt_effort,"COUNTRY", "2019","August", 9, "15-23")
-ggplot2::ggsave("2019_BtS_FO_Figure3.png", path = "~/", width = 178, height = 130, units = "mm", dpi = 300)
+plot_stecf(frmt_effort,type = "effort", variable= "COUNTRY", "2019","August", 9, "15-23", return_data = FALSE)
+frmt_effort <- dplyr::filter(frmt_effort, COUNTRY %in% c("Sweden", "Poland", "Germany", "Denmark", "Lithuania","Latvia"))
+plot_stecf(frmt_effort,type = "effort", variable= "COUNTRY", "2019","August", 9, "15-23", return_data = FALSE)
+ggplot2::ggsave("2019_BtS_FO_Figure3.png", path = "report/", width = 178, height = 130, units = "mm", dpi = 300)
         #data
-plot_dat <- plot_stecf(frmt_effort,"COUNTRY", "2019","August", 9, "15-23", return_data = TRUE)
-write.taf(plot_dat, file= "2019_BtS_FO_Figure3.csv", dir = "report")
+dat <- plot_stecf(frmt_effort,type = "effort", variable= "COUNTRY", "2019","August", 9, "15-23", return_data = TRUE)
+write.taf(dat, file= "2019_BtS_FO_Figure3.csv", dir = "report")
 
 
 #~~~~~~~~~~~~~~~#
 #Effort by gear
 #~~~~~~~~~~~~~~~#
         #Plot
-plot_stecf(frmt_effort,"GEAR", "2019","August", 9, "15-23")
-ggplot2::ggsave("2019_BtS_FO_Figure8.png", path = "~/", width = 178, height = 130, units = "mm", dpi = 300)
+plot_stecf(frmt_effort,type = "effort", variable= "GEAR", "2019","August", 9, "15-23")
+ggplot2::ggsave("2019_BtS_FO_Figure8.png", path = "report/", width = 178, height = 130, units = "mm", dpi = 300)
 
         #data
-plot_dat<-plot_stecf(frmt_effort,"GEAR", "2019","August", 9, "15-23", return_data = TRUE)
-write.taf(plot_dat, file= "B2019_BtS_FO_Figure8.csv", dir = "report")
+dat<-plot_stecf(frmt_effort,type = "effort", variable= "GEAR", "2019","August", 9, "15-23", return_data = TRUE)
+write.taf(dat, file= "B2019_BtS_FO_Figure8.csv", dir = "report")
 
 #~~~~~~~~~~~~~~~#
 #Landings by country
 #~~~~~~~~~~~~~~~#
         #Plot
-plot_stecf(frmt_landings,"GEAR", "2019","August", 9, "15-23")
-ggplot2::ggsave("2019_BtS_FO_Figure6.png", path = "~/", width = 178, height = 130, units = "mm", dpi = 300)
+plot_stecf(frmt_landings,type = "landings", variable= "GEAR", "2019","August", 9, "15-23")
+ggplot2::ggsave("2019_BtS_FO_Figure6.png", path = "report/", width = 178, height = 130, units = "mm", dpi = 300)
         #dat
-plot_dat<- plot_stecf(frmt_landings,"landings", variable, "2019","August", 9, "15-23", return_data = TRUE)
-write.taf(plot_dat, file= "2019_BtS_FO_Figure6.csv", dir = "report")
+dat<- plot_stecf(frmt_landings,type = "landings", variable="landings", variable, "2019","August", 9, "15-23", return_data = TRUE)
+write.taf(dat, file= "2019_BtS_FO_Figure6.csv", dir = "report")
 
 ###########
 ## 3: SAG #
@@ -103,26 +123,26 @@ write.taf(plot_dat, file= "2019_BtS_FO_Figure6.csv", dir = "report")
 # 1. Demersal
 #~~~~~~~~~~~
 plot_stock_trends(trends, guild="demersal", cap_year = 2019, cap_month = "August", return_data = FALSE)
-ggplot2::ggsave("2019_BtS_FO_Figure12b.png", path = "~/", width = 178, height = 130, units = "mm", dpi = 300)
+ggplot2::ggsave("2019_BtS_FO_Figure12b.png", path = "report/", width = 178, height = 130, units = "mm", dpi = 300)
 
-plot_dat <- plot_stock_trends(trends, guild="demersal", cap_year = 2019, cap_month = "August", return_data = TRUE)
-write.taf(plot_dat, file ="2019_BtS_FO_Figure12b.csv", dir = "report")
+dat <- plot_stock_trends(trends, guild="demersal", cap_year = 2019, cap_month = "August", return_data = TRUE)
+write.taf(dat, file ="2019_BtS_FO_Figure12b.csv", dir = "report")
 
 # 2. Pelagic
 #~~~~~~~~~~~
 plot_stock_trends(trends, guild="pelagic", cap_year = 2019, cap_month = "August", return_data = FALSE)
-ggplot2::ggsave("2019_BtS_FO_Figure12c.png", path = "~/", width = 178, height = 130, units = "mm", dpi = 300)
+ggplot2::ggsave("2019_BtS_FO_Figure12c.png", path = "report/", width = 178, height = 130, units = "mm", dpi = 300)
 
-plot_dat <- plot_stock_trends(trends, guild="pelagic", cap_year = 2018, cap_month = "November", return_data = T)
-write.taf(plot_dat,file ="2019_BtS_FO_Figure12c.csv", dir = "report")
+dat <- plot_stock_trends(trends, guild="pelagic", cap_year = 2018, cap_month = "November", return_data = T)
+write.taf(dat,file ="2019_BtS_FO_Figure12c.csv", dir = "report")
 
 # 3. Benthic
 #~~~~~~~~~~~
 plot_stock_trends(trends, guild="benthic", cap_year = 2019, cap_month = "August",return_data = FALSE )
-ggplot2::ggsave("2019_BtS_FO_Figure12a.png", path = "~/", width = 178, height = 130, units = "mm", dpi = 300)
+ggplot2::ggsave("2019_BtS_FO_Figure12a.png", path = "report/", width = 178, height = 130, units = "mm", dpi = 300)
 
-plot_dat <- plot_stock_trends(trends, guild="benthic", cap_year = 2018, cap_month = "November", return_data = TRUE)
-write.taf(plot_dat, file ="2019_BtS_FO_Figure12a.csv", dir = "report" )
+dat <- plot_stock_trends(trends, guild="benthic", cap_year = 2018, cap_month = "November", return_data = TRUE)
+write.taf(dat, file ="2019_BtS_FO_Figure12a.csv", dir = "report" )
 
 
 #~~~~~~~~~~~~~~~#
@@ -140,7 +160,7 @@ kobe <- plot_kobe(catch_current, guild = "demersal", caption = T, cap_year = 201
 #kobe_dat is just like bar_dat with one less variable
 #kobe_dat <- plot_kobe(catch_current, guild = "Demersal", caption = T, cap_year = 2018, cap_month = "November", return_data = T)
 
-png(paste0(output_path, "2019_BtS_FO_Figure13_demersal", ".png"),
+png(paste0("report/", "2019_BtS_FO_Figure13_demersal", ".png"),
     width = 131.32,
     height = 88.9,
     units = "mm",
@@ -158,7 +178,7 @@ bar_dat <- plot_CLD_bar(catch_current, guild = "pelagic", caption = T, cap_year 
 write.taf(bar_dat, file ="2019_BtS_FO_Figure13_pelagic.csv", dir = "report")
 
 kobe <- plot_kobe(catch_current, guild = "pelagic", caption = T, cap_year = 2019, cap_month = "August", return_data = F)
-png(paste0(output_path, "2019_BtS_FO_Figure13_pelagic", ".png"),
+png(paste0("report/", "2019_BtS_FO_Figure13_pelagic", ".png"),
     width = 131.32,
     height = 88.9,
     units = "mm",
@@ -177,7 +197,7 @@ bar_dat <- plot_CLD_bar(catch_current, guild = "benthic", caption = T, cap_year 
 write.taf(bar_dat, file ="2019_BtS_FO_Figure13_benthic.csv", dir = "report" )
 
 kobe <- plot_kobe(catch_current, guild = "benthic", caption = T, cap_year = 2018, cap_month = "August", return_data = F)
-png(paste0(output_path, "2019_BtS_FO_Figure13_benthic", ".png"),
+png(paste0("report/", "2019_BtS_FO_Figure13_benthic", ".png"),
     width = 131.32,
     height = 88.9,
     units = "mm",
@@ -196,7 +216,7 @@ bar_dat <- plot_CLD_bar(catch_current, guild = "All", caption = T, cap_year = 20
 write.taf(bar_dat, file ="2019_BtS_FO_Figure13_All.csv", dir = "report" )
 
 kobe <- plot_kobe(catch_current, guild = "All", caption = T, cap_year = 2018, cap_month = "August", return_data = F)
-png(paste0(output_path, "2019_BtS_FO_Figure13_All", ".png"),
+png(paste0("report/", "2019_BtS_FO_Figure13_All", ".png"),
     width = 131.32,
     height = 88.9,
     units = "mm",
@@ -212,16 +232,16 @@ dev.off()
 #~~~~~~~~~~~~~~~#
 discardsA <- plot_discard_trends(catch_trends, 2019, cap_year = 2019, cap_month = "August")
 
-plot_dat<- plot_discard_trends(catch_trends, 2019, cap_year = 2019, cap_month = "August", return_data = T)
-write.csv(plot_dat, file ="2019_BtS_FO_Figure7_trends.csv", dir = "report" )
+dat<- plot_discard_trends(catch_trends, 2019, cap_year = 2019, cap_month = "August", return_data = T)
+write.csv(dat, file ="2019_BtS_FO_Figure7_trends.csv", dir = "report" )
 
 #Need to change order?
 discardsB <- plot_discard_current(catch_trends, 2019, cap_year = 2019, cap_month = "August")
 
-plot_dat <- discardsB <- plot_discard_current(catch_trends, 2019, cap_year = 2018, cap_month = "August", return_data = T)
-write.taf(plot_dat, file ="2019_BtS_FO_Figure7_current.csv", dir = "report" )
+dat <- discardsB <- plot_discard_current(catch_trends, 2019, cap_year = 2018, cap_month = "August", return_data = T)
+write.taf(dat, file ="2019_BtS_FO_Figure7_current.csv", dir = "report" )
 
-png(paste0(output_path, "2019_BtS_FO_Figure7", ".png"),
+png(paste0("report/", "2019_BtS_FO_Figure7", ".png"),
     width = 131.32,
     height = 88.9,
     units = "mm",
@@ -236,26 +256,26 @@ dev.off()
 #~~~~~~~~~~~~~~~#
 
 plot_status_prop_pies(frmt_status, "August", "2019")
-ggplot2::ggsave("2019_BtS_FO_Figure10.png", path = "~/", width = 178, height = 178, units = "mm", dpi = 300)
+ggplot2::ggsave("2019_BtS_FO_Figure10.png", path = "report/", width = 178, height = 178, units = "mm", dpi = 300)
 
-plot_dat <- plot_status_prop_pies(frmt_status, "November", "2018", return_data = T)
-write.taf(plot_dat, file= "2019_BtS_FO_Figure10.csv", dir = "report")
+dat <- plot_status_prop_pies(frmt_status, "November", "2018", return_data = T)
+write.taf(dat, file= "2019_BtS_FO_Figure10.csv", dir = "report")
 
 #~~~~~~~~~~~~~~~#
 #E. GES pies
 #~~~~~~~~~~~~~~~#
 #Need to change order and fix numbers
 plot_GES_pies(frmt_status, catch_current, "August", "2019")
-ggplot2::ggsave("2019_BtS_FO_Figure11.png", path = "~/", width = 178, height = 178, units = "mm", dpi = 300)
+ggplot2::ggsave("2019_BtS_FO_Figure11.png", path = "report/", width = 178, height = 178, units = "mm", dpi = 300)
 
-plot_dat <- plot_GES_pies(frmt_status, catch_current, "November", "2018", return_data = T)
-write.taf(plot_dat, file= "2019_BtS_FO_Figure11.csv", dir = "report")
+dat <- plot_GES_pies(frmt_status, catch_current, "November", "2018", return_data = T)
+write.taf(dat, file= "2019_BtS_FO_Figure11.csv", dir = "report")
 
 #~~~~~~~~~~~~~~~#
 #F. ANNEX TABLE 
 #~~~~~~~~~~~~~~~#
 doc <- format_annex_table(frmt_status, 2019, return_data = F)
-print(doc, target = paste0("~/", "2019_BtS_FO_annex_table", ".docx"))
+print(doc, target = paste0("report/", "2019_BtS_FO_annex_table", ".docx"))
 
 dat <- format_annex_table(frmt_status, 2019, return_data = T)
 
