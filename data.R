30475014291b346de1e1fb8e71bb9306364b9458
Colin Millar <colin.millar@ices.dk>
2019-10-06 20:11:27 +0200

    cleaned and updated data script

--- data.R
+++ data.R
@@ -2,8 +2,13 @@
 # Initial formatting of the data
 
 taf.library(icesFO)
-require(dplyr)
+library(dplyr)
+
 mkdir("data")
+
+# load species list
+species_list <- read.taf("bootstrap/data/FAO_ASFIS_species/species_list.csv")
+sid <- read.taf("bootstrap/data/ICES_StockInformation/sid.csv")
 
 # 1: ICES official cath statistics
 
@@ -11,45 +16,36 @@ hist <- read.taf("bootstrap/data/ICES_nominal_catches/ICES_historical_catches.cs
 official <- read.taf("bootstrap/data/ICES_nominal_catches/ICES_2006_2017_catches.csv")
 prelim <- read.taf("bootstrap/data/ICES_nominal_catches/ICES_preliminary_catches.csv")
 
-catch_dat <- icesFO::format_catches(2019, "Baltic Sea Ecoregion", hist, official, prelim)
+catch_dat <- 
+  format_catches(2019, "Baltic Sea", 
+    hist, official, prelim, species_list, sid)
 
-write.taf(catch_dat, file = "data/catch_dat.csv", quote = TRUE)
+write.taf(catch_dat, dir = "data", quote = TRUE)
 
 # 2: STECF effort and landings
 
-effort <- read.taf("bootstrap/data/STECF_effort.csv")
-library(readr)
-landings <- read_csv("bootstrap/initial/data/STECF_landings.csv")
+effort <- read.taf("bootstrap/data/STECF_effort.csv", check.names = TRUE)
+
+landings <- read.taf("bootstrap/initial/data/STECF_landings.csv", check.names = TRUE)
 
 frmt_effort <- format_stecf_effort(effort)
-effort <- dplyr::rename(effort,
-                 regulated.area = `regulated area`,
-                 regulated.gear = `regulated gear`)
-frmt_effort <- format_stecf_effort(effort)
-frmt_landings <- format_stecf_landings(landings)
-names(landings)
-landings <- dplyr::rename(landings,
-                        regulated.area = `regulated area`,
-                        regulated.gear = `regulated gear`)
 frmt_landings <- format_stecf_landings(landings)
 
-write.taf(frmt_effort, file = "data/frmt_effort.csv", quote = TRUE)
-write.taf(frmt_landings, file = "data/frmt_landings.csv")
+write.taf(frmt_effort, dir = "data", quote = TRUE)
+write.taf(frmt_landings, dir = "data")
 
 # 3: SAG
 sag_sum <- read.taf("bootstrap/data/SAG_data/SAG_summary.csv")
 sag_refpts <- read.taf("bootstrap/data/SAG_data/SAG_refpts.csv")
 sag_status <- read.taf("bootstrap/data/SAG_data/SAG_status.csv")
 
-Baltic_Out_stocks <-  c("sal.27.32", "sal.27.22-31", "ele.2737.nea", "trs.27.22-32", "her.27.30", "her.27.31")
-
 clean_sag <- format_sag(sag_sum, sag_refpts, 2019, "Baltic")
 clean_status <- format_sag_status(sag_status, 2019, "Baltic Sea")
 
-library(operators)
-clean_sag <- dplyr::filter(clean_sag, StockKeyLabel %!in% Baltic_Out_stocks)
-clean_status <- dplyr::filter(clean_status, StockKeyLabel %!in% Baltic_Out_stocks)
-detach("package:operators", unload=TRUE)
+# remove some stocks
+Baltic_Out_stocks <-  c("sal.27.32", "sal.27.22-31", "ele.2737.nea", "trs.27.22-32", "her.27.30", "her.27.31")
+clean_sag <- dplyr::filter(clean_sag, !StockKeyLabel %in% Baltic_Out_stocks)
+clean_status <- dplyr::filter(clean_status, !StockKeyLabel %in% Baltic_Out_stocks)
 
-write.taf(clean_sag, file = "data/clean_sag.csv")
-write.taf(clean_status, file = "data/clean_status.csv")
+write.taf(clean_sag, dir = "data")
+write.taf(clean_status, dir = "data")
