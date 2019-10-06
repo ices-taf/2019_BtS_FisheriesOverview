e555f2107e422092c149ebbc42bd3d9c06e81959
Colin Millar <colin.millar@ices.dk>
2019-10-06 22:03:13 +0200

    name change in load_ecoregion()

--- bootstrap/ICES_ecoregions.R
+++ bootstrap/ICES_ecoregions.R
@@ -1,6 +1,6 @@
 library(icesTAF)
 taf.library(icesFO)
 
-ecoregions <- icesFO::load_ecoregions("Baltic Sea")
+ecoregion <- icesFO::load_ecoregion("Baltic Sea")
 
-sf::st_write(ecoregions, "ecoregions.csv", layer_options = "GEOMETRY=AS_WKT")
+sf::st_write(ecoregion, "ecoregion.csv", layer_options = "GEOMETRY=AS_WKT")
