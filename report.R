934f47ea07aebaf9e1514aaaa9dcbca723bde466
Adriana Villamor <adriana.villamor@ices.dk>
2019-11-15 11:59:22 +0100

    EO graphs with no mean

--- report.R
+++ report.R
@@ -172,6 +172,11 @@ guild <- read.taf("model/guild.csv")
 
 plot_guild_trends(guild, cap_year = 2019, cap_month = "October",return_data = FALSE )
 ggplot2::ggsave("2019_BtS_EO_GuildTrends.png", path = "report/", width = 178, height = 130, units = "mm", dpi = 300)
+guild2 <- guild %>% filter(FisheriesGuild != "MEAN")
+plot_guild_trends(guild2, cap_year = 2019, cap_month = "November",return_data = FALSE )
+ggplot2::ggsave("2019_BtS_EO_GuildTrends_noMEAN.png", path = "report/", width = 178, height = 130, units = "mm", dpi = 300)
+
+
 
 dat <- plot_guild_trends(guild, cap_year = 2019, cap_month = "October",return_data = TRUE)
 write.taf(dat, file ="2019_BtS_EO_GuildTrends.csv", dir = "report" )
@@ -179,6 +184,8 @@ write.taf(dat, file ="2019_BtS_EO_GuildTrends.csv", dir = "report" )
 dat <- trends[,1:2]
 dat <- unique(dat)
 dat <- dat %>% filter(StockKeyLabel != "MEAN")
+dat2 <- sid %>% select(c(StockKeyLabel, StockKeyDescription))
+dat <- left_join(dat,dat2)
 write.taf(dat, file ="2019_BtS_EO_SpeciesGuild_list.csv", dir = "report" )
 
 #~~~~~~~~~~~~~~~#
