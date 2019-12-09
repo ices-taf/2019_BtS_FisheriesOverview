58dd84fb972306392bd0d93bb4153de44578dfed
Adriana Villamor <adriana.villamor@ices.dk>
2019-12-09 14:42:53 +0100

    separated EOs graphs

--- report.R
+++ report.R
@@ -174,12 +174,19 @@ write.taf(dat, file ="2019_BtS_FO_Figure12a.csv", dir = "report" )
 #~~~~~~~~~~~~~~~~~~~~~~~~~#
 guild <- read.taf("model/guild.csv")
 
+# For this EO, they need separate plots with all info
+
+guild2 <- guild %>% filter(Metric == "F_FMSY")
 plot_guild_trends(guild, cap_year = 2019, cap_month = "October",return_data = FALSE )
 ggplot2::ggsave("2019_BtS_EO_GuildTrends.png", path = "report/", width = 178, height = 130, units = "mm", dpi = 300)
-guild2 <- guild %>% filter(FisheriesGuild != "MEAN")
+guild2 <- guild2 %>% filter(FisheriesGuild != "MEAN")
 plot_guild_trends(guild2, cap_year = 2019, cap_month = "November",return_data = FALSE )
-ggplot2::ggsave("2019_BtS_EO_GuildTrends_noMEAN.png", path = "report/", width = 178, height = 130, units = "mm", dpi = 300)
+ggplot2::ggsave("2019_BtS_EO_GuildTrends_noMEAN_F.png", path = "report/", width = 178, height = 130, units = "mm", dpi = 300)
 
+guild2 <- guild %>% filter(Metric == "SSB_MSYBtrigger")
+guild3 <- guild2 %>% dplyr::filter(FisheriesGuild != "MEAN")
+plot_guild_trends(guild3, cap_year = 2019, cap_month = "November",return_data = FALSE )
+ggplot2::ggsave("2019_BtS_EO_GuildTrends_short_noMEAN_SSB.png", path = "report/", width = 178, height = 130, units = "mm", dpi = 300)
 
 
 dat <- plot_guild_trends(guild, cap_year = 2019, cap_month = "October",return_data = TRUE)
